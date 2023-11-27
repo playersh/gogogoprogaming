2fb9fd6bdabf5afc7f58a9e34fdd9dd68502bded
cshcdh123 <cshcdh123@gmail.com>
2023-11-27 14:45:21 +0900

    week14

---
+++ Desktop/realgo/week14/main1.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func status(name string) {
+	balls := map[string]int{"성기훈": 20, "오일남": 0}
+	// ball := balls[name]
+	// fmt.Println(ball)
+	ball, exists := balls[name]
+	if !exists {
+		fmt.Println(name, "님은 게임 참여자가 아닙니다.")
+	} else if ball < 1 {
+		fmt.Println(name, "님은", balls[name], "개로 탈락")
+	} else {
+		fmt.Println(name, "님은 게임에서 승리하셨습니다.")
+	}
+}
+
+func main() {
+	status("오일남")
+	status("강철")
+	status("성기훈")
+}
